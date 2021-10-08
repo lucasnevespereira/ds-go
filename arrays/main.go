2e3bfafd3a828837ab7d2cfeb8de87000cefaa65
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-10-09 00:48:46 +0200

    :sparkles: arrays and slices

--- arrays/main.go
+++ arrays/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	// fixed len
+	var myArray [8]int
+	fmt.Println(myArray)
+
+	// non-fixed len
+	var mySlice []int
+	mySlice = append(mySlice, 1, 2, 3, 4)
+	fmt.Println(mySlice)
+}
